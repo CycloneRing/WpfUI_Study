acde6b82e7c1efc49e26fdc6885a1a57bc2de3ce
Cyclone <91777373+CycloneRing@users.noreply.github.com>
2024-01-03 14:51:33 +0330

    Add files via upload

---
+++ Dashboard.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Interfaces;
+using Wpf.Ui.TaskBar;
+using WpfUIServices = Wpf.Ui.Mvvm.Services;
+
+namespace WpfUIStudy
+{
+    public partial class Dashboard : INavigationWindow
+    {
+        private readonly IThemeService      _themeService;
+        private readonly ITaskBarService    _taskBarService;
+        private readonly INavigationService _navigationService;
+
+        public Frame GetFrame() => RootFrame;
+        public INavigation GetNavigation() => RootNavigation;
+        public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
+        public void SetPageService(IPageService pageService) => RootNavigation.PageService = pageService;
+        public void ShowWindow() => Show();
+        public void CloseWindow() => Close();
+
+        // Initializer
+        public Dashboard()
+        {
+            _themeService = new WpfUIServices.ThemeService();
+            _taskBarService = new WpfUIServices.TaskBarService();
+            _navigationService = new WpfUIServices.NavigationService();
+
+            InitializeComponent();
+        }
+
+        // Events
+        private void ThemeSwitchClick(object sender, RoutedEventArgs e)
+        {
+            _themeService.SetTheme(_themeService.GetTheme() == ThemeType.Dark ? ThemeType.Light : ThemeType.Dark);
+        }
+        private void DashboardLoaded(object sender, RoutedEventArgs e)
+        {
+            // Set System Accent Color
+            _themeService.SetSystemAccent();
+
+            // Load Default Page
+            DefaultNavigation.RaiseEvent(new RoutedEventArgs(NavigationItem.ClickEvent));
+            Navigate(typeof(Pages.Home));
+        }
+    }
+}
---
+++ Pages/Home.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Controls;
+
+public class HomeViewModel : INavigationAware 
+{
+    public void OnNavigatedTo()
+    {
+    }
+    public void OnNavigatedFrom()
+    {
+    }
+}
+namespace WpfUIStudy.Pages
+{
+    public partial class Home : INavigableView<HomeViewModel>
+    {
+        public HomeViewModel ViewModel { get; }
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
